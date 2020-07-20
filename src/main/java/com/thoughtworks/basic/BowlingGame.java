1dc586db7ddc65995cbba2b85dbfc4998082b2dc
lw <623656827@qq.com>
2020-07-20 18:36:40 +0800

    初始化 代码

---
+++ src/main/java/com/thoughtworks/basic/BowlingGame.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.basic;
+
+import java.util.List;
+
+public class BowlingGame {
+    private List<Frame> frames;
+    private Frame currentFrame;
+    public void roll(int hits){
+
+    }
+    public int getScore(){
+        return 0;
+    }
+    public boolean isEnd(){
+        return false;
+    }
+    public String showFramesScore(){
+        return null;
+    }
+}
---
+++ src/main/java/com/thoughtworks/basic/Frame.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.basic;
+
+import java.util.List;
+
+public class Frame {
+    private List<Integer> rolls;
+    public void roll(int hits){
+
+    }
+    public int getScore(){
+        return 0;
+    }
+    public boolean isEnd(){
+        return false;
+    }
+}
